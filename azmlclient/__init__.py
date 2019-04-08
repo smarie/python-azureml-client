edba5f21ad2961824a82da43c43388717920dbd5
Sylvain MARIE <sylvain.marie@se.com>
2019-04-08 10:51:17 +0200

    Fixed init file with explicit names (using * is not recommended).

--- azmlclient/__init__.py
+++ azmlclient/__init__.py
@@ -1,6 +1,12 @@
-from azmlclient.data_binding import *
-from azmlclient.service_calls import *
+from azmlclient.data_binding import AzmlException, Converters, ByReference_Converters, Collection_Converters
+from azmlclient.service_calls import execute_rr, execute_bes, IllegalJobStateException, JobExecutionException, \
+    create_session_for_proxy, RR_Client, Batch_Client
 
-__all__ = [data_binding, service_calls]
-
-
+__all__ = [
+    # submodules
+    'data_binding', 'service_calls',
+    # symbols imported above
+    'AzmlException', 'Converters', 'ByReference_Converters', 'Collection_Converters',
+    'execute_rr', 'execute_bes', 'IllegalJobStateException', 'JobExecutionException', 'create_session_for_proxy',
+    'RR_Client', 'Batch_Client'
+]
