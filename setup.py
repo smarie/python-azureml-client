cec19199767979f113a4cda00e3b8ccc914c44bf
Sylvain MARIE <sylvain.marie@se.com>
2019-04-08 11:32:25 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,49 +3,57 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
-
+from six import raise_from
 from os import path
 
-from setuptools import setup
-from setuptools_scm import get_version
+from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
+
+# *************** Dependencies *********
+INSTALL_REQUIRES = ['azure-storage==0.33.0']
+DEPENDENCY_LINKS = []
+SETUP_REQUIRES = ['pytest-runner', 'setuptools_scm', 'pypandoc', 'pandoc']
+TESTS_REQUIRE = ['pytest', 'pytest-logging', 'pytest-cov']
+EXTRAS_REQUIRE = {}
+
+# simple check
+try:
+    from setuptools_scm import get_version
+except Exception as e:
+    raise_from(Exception('Required packages for setup not found. Please install `setuptools_scm`'), e)
 
 # ************** ID card *****************
 DISTNAME = 'azmlclient'
 DESCRIPTION = 'A Python 3 client for AzureMl web services'
-MAINTAINER = 'Sylvain Marié'
-MAINTAINER_EMAIL = '"Sylvain Marié" <sylvain.marie@schneider-electric.com>'
+MAINTAINER = 'Sylvain MARIE'
+MAINTAINER_EMAIL = 'sylvain.marie@schneider-electric.com'
 URL = 'https://github.com/smarie/python-azureml-client'
 LICENSE = 'MIT'
+LICENSE_LONG = 'License :: OSI Approved :: MIT License'
 
-version_for_download_url = get_version()#local_scheme='dirty-tag')
+version_for_download_url = get_version()
+DOWNLOAD_URL = URL + '/tarball/' + version_for_download_url
 
-DOWNLOAD_URL = 'https://github.com/smarie/python-azureml-client/tarball/' + version_for_download_url
 KEYWORDS = 'Azure Machine Learning AzureML web services client'
 # --Get the long description from the README file
-#with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 #    LONG_DESCRIPTION = f.read()
 try:
     import pypandoc
-    LONG_DESCRIPTION = pypandoc.convert(path.join(here, 'README.md'), 'rst').replace('\r', '')
+    LONG_DESCRIPTION = pypandoc.convert(path.join(here, 'docs', 'long_description.md'), 'rst').replace('\r', '')
 except(ImportError):
     from warnings import warn
     warn('WARNING pypandoc could not be imported - we recommend that you install it in order to package the '
          'documentation correctly')
     LONG_DESCRIPTION = open('README.md').read()
 
-# ************* VERSION AND DEPENDENCIES **************
+# ************* VERSION **************
 # --Get the Version number from VERSION file, see https://packaging.python.org/single_source_version/ option 4.
 # THIS IS DEPRECATED AS WE NOW USE GIT TO MANAGE VERSION
 # with open(path.join(here, 'VERSION')) as version_file:
 #    VERSION = version_file.read().strip()
-
-INSTALL_REQUIRES = ['numpy', 'pandas', 'azure-storage==0.33.0']
-DEPENDENCY_LINKS = []
-SETUP_REQUIRES = ['setuptools_scm','pypandoc']
-TESTS_REQUIRE = ['nose']
-EXTRAS_REQUIRE = {}
+# OBSOLETES = []
 
 setup(
     name=DISTNAME,
@@ -55,7 +63,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    #version=VERSION, NOW HANDLED BY GIT
+    # version=VERSION, NOW HANDLED BY GIT
 
     maintainer=MAINTAINER,
     maintainer_email=MAINTAINER_EMAIL,
@@ -70,24 +78,26 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        LICENSE_LONG,
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        #'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.6',
-        #'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
@@ -95,7 +105,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['azmlclient'], #find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -110,18 +120,20 @@ setup(
 
     # we're using git
     use_scm_version=True, # this provides the version + adds the date if local non-commited changes.
-    #use_scm_version={'local_scheme':'dirty-tag'}, # this provides the version + adds '+dirty' if local non-commited changes.
+    # use_scm_version={'local_scheme':'dirty-tag'}, # this provides the version + adds '+dirty' if local non-commited changes.
     setup_requires=SETUP_REQUIRES,
 
     # test
-    test_suite='nose.collector',
+    # test_suite='nose.collector',
     tests_require=TESTS_REQUIRE,
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require=EXTRAS_REQUIRE
+    extras_require=EXTRAS_REQUIRE,
+
+    # obsoletes=OBSOLETES
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
