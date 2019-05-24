bcddbee3ec0281acd808f6b62ee439c0efade8d2
Sylvain MARIE <sylvain.marie@se.com>
2019-05-24 18:10:44 +0200

    Removed useless code.

--- azmlclient/utils_testing.py
+++ azmlclient/utils_testing.py
@@ -1,7 +1,4 @@
-import csv
-import pandas as pd
-
-from azmlclient.base_databinding import create_dest_buffer_for_csv, create_reading_buffer, azmltable_to_df
+from azmlclient.base_databinding import azmltable_to_df
 
 
 class AzuremlWebServiceMock(object):
@@ -35,43 +32,6 @@ class AzuremlWebServiceMock(object):
             if input_name not in timeseries_input_names and input_name not in table_input_names:
                 raise ValueError("Invalid input received. Supported inputs are: %s and %s" % (timeseries_input_names,
                                                                                               table_input_names))
-            input_dfs[input_name] = convert_dct_table_to_df(input_name, dict_table,
-                                                            is_timeseries=input_name in timeseries_input_names)
+            input_dfs[input_name] = azmltable_to_df(dict_table, is_azml_output=False, table_name=input_name)
 
         return input_dfs, global_params_dct
-
-
-def convert_dct_table_to_df(input_name, dict_table, is_timeseries):
-    """
-    A fix for Converters.azmltable_to_df
-
-    :param input_name:
-    :param dict_table:
-    :param is_timeseries:
-    :return:
-    """
-    if not is_timeseries:
-        # since the underlying library does not handle it, we artificially add a column.
-        if 'ColumnNames' in dict_table.keys() and 'Values' in dict_table.keys():
-            values = dict_table['Values']
-            if len(values) > 0:
-                # use pandas parser to infer most of the types
-                # -- for that we first dump in a buffer in a CSV format
-                buffer = create_dest_buffer_for_csv()
-                writer = csv.writer(buffer, dialect='unix')
-                writer.writerows([dict_table['ColumnNames']])
-                writer.writerows(values)
-                # -- and then we parse with pandas
-                res = pd.read_csv(create_reading_buffer(buffer.getvalue()), sep=',', decimal='.')
-                buffer.close()
-                return res
-            else:
-                # empty dataframe
-                return pd.DataFrame(columns=dict_table['ColumnNames'])
-        else:
-            raise ValueError(
-                'object should be a dictionary with two fields ColumnNames and Values, found: ' + str(
-                    dict_table.keys()) + ' for table object: ' + input_name)
-    else:
-        # there is a datetime as first column, this is ok
-        return azmltable_to_df(dict_table, is_azml_output=False, table_name=input_name)
